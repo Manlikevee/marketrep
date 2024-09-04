de81998584ddcf8962c7c6e97a01482e407da787
manlikevee <odahviktor@gmail.com>
2024-09-04 13:46:24 +0100

    new proj

--- build_files.sh
+++ build_files.sh
@@ -9,8 +9,8 @@ source venv/bin/activate
 # install all deps in the venv
 pip install -r requirements.txt
 
-# collect static files using the Python interpreter from venv
-python manage.py collectstatic --noinput
+## collect static files using the Python interpreter from venv
+#python manage.py collectstatic --noinput
 
 echo "BUILD END"
 
