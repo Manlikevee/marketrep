3bb4fe10ef0db261c9d2ebbfb051c2d008da0d39
manlikevee <odahviktor@gmail.com>
2024-09-04 14:00:28 +0100

    new proj

--- build_files.sh
+++ build_files.sh
@@ -1,15 +1,15 @@
 echo "BUILD START"
 
 # create a virtual environment named 'venv' if it doesn't already exist
-python3.9 -m venv venv
+python3.12 -m venv venv
 
 # activate the virtual environment
 source venv/bin/activate
 
 
-pip install --upgrade pip
-# install all deps in the venv
-pip install --upgrade -r requirements.txt
+#pip install --upgrade pip
+## install all deps in the venv
+#pip install --upgrade -r requirements.txt
 
 ## collect static files using the Python interpreter from venv
 #python manage.py collectstatic --noinput
