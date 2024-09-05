d6becf01a52f4c2a607f0b6d83f3a4c3b4374156
manlikevee <odahviktor@gmail.com>
2024-09-05 11:56:10 +0100

    new proj

--- build_files.sh
+++ build_files.sh
@@ -3,19 +3,16 @@ echo "BUILD START"
 # create a virtual environment named 'venv' if it doesn't already exist
 python3.9 -m venv venv
 
-
 # activate the virtual environment
 source venv/bin/activate
 
-
 # install all deps in the venv
 pip install -r requirements.txt
-python3.9 manage.py runserver
 
+# collect static files using the Python interpreter from venv
+python manage.py collectstatic --noinput
 
 echo "BUILD END"
 
-# [optional] Start the application here 
+# [optional] Start the application here
 # python manage.py runserver
-
-#2012 6 months renew $150    2019 950 + 250 = 1200
--- home/apps.py
+++ home/apps.py
@@ -8,10 +8,10 @@ class HomeConfig(AppConfig):
     def ready(self):
         from .views import run_table_7_data_check, run_table_8_data_check, run_table_9_data_check, run_table_12_data_check
 
-        # # Run the view
-        # run_table_7_data_check()
-        # run_table_8_data_check()
-        # run_table_9_data_check()
-        # run_table_12_data_check()
+        # Run the view
+        run_table_7_data_check()
+        run_table_8_data_check()
+        run_table_9_data_check()
+        run_table_12_data_check()
 
 
